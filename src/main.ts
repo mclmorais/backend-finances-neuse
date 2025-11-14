02e4b76cb852533506d8ad7768e28f7355b21b67
Marcelo Fernandes <marcelodotmorais@gmail.com>
2025-11-13 22:06:16 -0300

    nestjs-zod addition

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { cleanupOpenApiDoc } from 'nestjs-zod';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const openApiDoc = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Backend Finances Neuse API')
+      .setDescription('API documentation for Backend Finances Neuse')
+      .setVersion('1.0')
+      .build(),
+  );
+
+  SwaggerModule.setup('api', app, cleanupOpenApiDoc(openApiDoc));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
