2e0750a131e0a08cad3f18825f9be30ff3dbd5bc
Marcelo Fernandes <marcelodotmorais@gmail.com>
2025-11-16 17:15:57 -0300

    3001 port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { cleanupOpenApiDoc } from 'nestjs-zod';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const openApiDoc = SwaggerModule.createDocument(
     app,
@@ -45,6 +45,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
