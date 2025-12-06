814c3812ce45098a002f74b40a4ab301336d83de
Marcelo Fernandes <marcelodotmorais@gmail.com>
2025-12-06 09:21:25 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -45,6 +45,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000, '192.168.15.10');
+  await app.listen(process.env.PORT ?? 3000, '192.168.15.14');
 }
 void bootstrap();
